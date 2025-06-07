a8fcae72f98a63ac3a848a877693d7268b3d40c4
Harshith <harshith.1si20sfc03@gmail.com>
2025-06-08 01:48:01 +0530

    Day2 incomplete

--- Python/Python_Basic/Hello.py
+++ Python/Python_Basic/Hello.py
@@ -1,3 +1,6 @@
 # This program prints Hello, world!
 
 print('Hello, world!')
+print ('hello, Harith')
+print('hello 123')
+print(15)
---
+++ Python/Python_Basic/Operators.py
@@ -0,0 +1,11 @@
+print(2+2)
+
+# We can print multiple items by separating by commas.
+
+print(2+5, 2*4, 2/2, 4-1)
+
+print(11*13)          # Area of the rectangle = l*b
+
+print(2* (11+13))    # Perimeter of the rectangle = 2(l+b)
+
+print(13, end="")
